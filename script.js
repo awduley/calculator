4f55767919f89418cee70cc9f93c43cdddba5e4c
awduley <duleyalaska@gmail.com>
2025-04-19 13:37:54 -0700

    Complete the layout of the calculator
    The layout for the calculator is finished, and
    I'll begin working on the logic of the app

---
+++ script.js
@@ -0,0 +1,23 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+let num1;
+let operator;
+let num2;
+
+function operate(num1, operator, num2) {
+
+}
